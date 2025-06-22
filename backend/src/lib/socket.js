f1e8bf9f70db402947ee8adbb1af0d53c98d85e9
Archita Saraswati <architasaraswati@gmail.com>
2025-06-22 23:56:10 +0530

    refactor:

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,13 +1,48 @@
 
 
 
+// import { Server } from "socket.io";
+
+// let io;
+// const userSocketMap = {}; // { userId: socketId }
+
+// function initializeSocket(server) {
+//   io = new Server(server, {
+//     cors: {
+//       origin: ["http://localhost:5173"],
+//     },
+//   });
+
+//   io.on("connection", (socket) => {
+//     console.log("A user connected", socket.id);
+
+//     const userId = socket.handshake.query.userId;
+//     if (userId) userSocketMap[userId] = socket.id;
+
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//     socket.on("disconnect", () => {
+//       console.log("A user disconnected", socket.id);
+//       delete userSocketMap[userId];
+//       io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//     });
+//   });
+// }
+
+// function getReceiverSocketId(userId) {
+//   return userSocketMap[userId];
+// }
+
+// export { initializeSocket, getReceiverSocketId };
+
+
 import { Server } from "socket.io";
 
 let io;
 const userSocketMap = {}; // { userId: socketId }
 
 function initializeSocket(server) {
-  io = new Server(server, {
+  io = nsew Server(server, {
     cors: {
       origin: ["http://localhost:5173"],
     },
@@ -33,4 +68,8 @@ function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-export { initializeSocket, getReceiverSocketId };
+function getIO() {
+  return io;
+}
+
+export { initializeSocket, getReceiverSocketId, getIO };
